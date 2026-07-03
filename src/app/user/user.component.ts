d1ec4a2eb85fc060d6a0923d8876794c3b58bc53
Faeze Rahmati <rahmati.faeze1997@gmail.com>
2026-07-03 19:43:39 +0330

    refactor(user): replace dynamic user generation with static user list
    - Remove random user selection logic from UserComponent
    - Add static user list in AppComponent
    - Update UserComponent to use inputs for avatar and name
    - Adjust HTML templates to reflect these changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, computed, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,18 +8,16 @@ const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomUser]);
-  imagePath = computed(
-    () => `../../assets/users/${this.selectedUser().avatar}`,
-  );
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  // get imagePath() {
-  //   return `../../assets/users/${this.selectedUser.avatar}`;
-  // }
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
-  onSelectUser() {
-    const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomUser]);
-    // this.selectedUser = DUMMY_USERS[randomUser];
+  // imagePath = computed(() => `../../assets/users/${this.avatar()}`);
+
+  get imagePath() {
+    return `../../assets/users/${this.avatar}`;
   }
+  onSelectUser() {}
 }
