d5e5970c712d422c62ba0c0d9a7fb5e17b000384
Faeze Rahmati <rahmati.faeze1997@gmail.com>
2026-07-06 01:12:59 +0330

    feat(tasks): implement local storage for tasks
    - Add constructor to load tasks from local storage
    - Implement saveTasks method to persist tasks
    - Update addTask and removeTask methods to save tasks after modification

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -28,6 +28,12 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
   getUsersTask(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -40,9 +46,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
